ff13a4629fdf406762cf41bc122b7d2177e593b1
Jayaneththi <udashifin@outlook.com>
2024-02-02 12:44:04 +0200

    modified

--- udashi_task/footer.php
+++ udashi_task/footer.php
@@ -1,8 +1,8 @@
 <footer>
     <div class="row">
-        <div class="col-sm-2">
+        <div class="col-sm-1">
             <img class="footerlogo" src="logo-4.png">
-        <img src="footerbanner.jpg" alt="footerbanner">
+        
      </div>
 
         </footer> 
