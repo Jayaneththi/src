5b717d16eb7ed70a9329ec0c7ccfe120015f445b
Jayaneththi <udashifin@outlook.com>
2024-02-02 11:56:39 +0200

    modified

--- udashi_task/header.php
+++ udashi_task/header.php
@@ -3,7 +3,9 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Asian Delight Restaurant</title>
+    <title>
+      <?php echo $title; ?>
+    </title>
     <link rel="icon" href="asiandelight.png">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -23,19 +25,19 @@
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav mx-auto">
               <li class="nav-item">
-                <a class="nav-link menu-active" aria-current="page" href="#home">Home</a>
+                <a class="nav-link menu-active" aria-current="page" href="ex1.php">Exercise1</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#about">About</a>
+                <a class="nav-link" href="ex2.php">Exercise2</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#menu">Menu</a>
+                <a class="nav-link" href="ex3.php">Exercise3</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#service">Services and Offers</a>
+                <a class="nav-link" href="ex4.php">Exercise4</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#contactUs">Contact Us</a>
+                <a class="nav-link" href="#">Exercise5</a>
               </li>
              
             </ul>
